86eca10441c184ee4b80500787af4843df6a34a3
rakhmanhanna <rakhmanhanna@gmail.com>
2023-02-24 17:02:10 +0100

    feat: submit task 1

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 'use strict';
 
-// const time = document.querySelector('.time');
+const time = document.querySelector('.time');
 // time.textContent = 'Good Evening';
 const greeting = document.querySelector('.greeting');
 greeting.textContent = 'Good Evening,';
-
-// const greeting-container = document.querySelector('.greeting-container');
-// greeting-container.textContent = '[Enter name]';
 
 const date = new Date();
 const currentTime = date.toLocaleTimeString();
@@ -17,11 +14,14 @@ const currentDate = date.toLocaleDateString('en-Us', options);
 document.querySelector('.date').innerHTML = currentDate;
 
 function showTime() {
+  const date = new Date();
+  const currentTime = date.toLocaleTimeString();
+  document.querySelector('.time').innerHTML = currentTime;
   return currentTime, setTimeout(showTime, 1000);
 }
 showTime();
 
 function showDate() {
-  return currentDate, setTimeout(showDate, 1000);
+  return currentDate;
 }
 console.log(showDate());
